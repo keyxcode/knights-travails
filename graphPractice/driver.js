a5c08d0e32b9d805a248f21594b7a51a92995216
keyxcode <keyxcode@gmail.com>
2023-03-23 17:36:43 +0700

    Implement a simple undirected graph class

---
+++ graphPractice/driver.js
@@ -0,0 +1,27 @@
+const Graph = require("./graph");
+
+const g = new Graph(6);
+const vertices = ["A", "B", "C", "D", "E", "F"];
+
+// adding vertices
+vertices.forEach((vertex) => g.addVertex(vertex));
+
+// adding edges
+g.addEdge("A", "B");
+g.addEdge("A", "D");
+g.addEdge("A", "E");
+g.addEdge("B", "C");
+g.addEdge("D", "E");
+g.addEdge("E", "F");
+g.addEdge("E", "C");
+g.addEdge("C", "F");
+
+// prints all vertex and
+// its adjacency list
+// A -> B D E
+// B -> A C
+// C -> B E F
+// D -> A E
+// E -> A D F C
+// F -> E C
+g.printGraph();
--- graphPractice/graph.js
+++ graphPractice/graph.js
@@ -4,16 +4,38 @@ class Graph {
     // Map is an ES6 object
     // here we use the key of the map to hold a vertex
     // and the value holds an array of an adjacent nodes
-    this.AdjList = new Map();
-
-    function addVertex(v) {}
-
-    function addEdge(v, w) {}
-
-    function printGraph() {}
-
-    function bfs() {}
-
-    function dfs() {}
+    this.adjList = new Map();
   }
+
+  addVertex(v) {
+    // init a vertex with no connection to any other nodes
+    this.adjList.set(v, []);
+  }
+
+  addEdge(v, dest) {
+    // create undirected connections between 2 vertices
+    this.adjList.get(v).push(dest);
+    this.adjList.get(dest).push(v);
+  }
+
+  printGraph() {
+    // get all the vertices
+    const vertices = this.adjList.keys();
+
+    for (const vertex of vertices) {
+      // get the corresponding adjacencies for each vertex
+      const adjacencies = this.adjList.get(vertex);
+      let printedAdjacencies = "";
+      adjacencies.forEach(
+        (adjacency) => (printedAdjacencies += `${adjacency} `)
+      );
+      console.log(vertex + " -> " + printedAdjacencies);
+    }
+  }
+
+  bfs() {}
+
+  dfs() {}
 }
+
+module.exports = Graph;
