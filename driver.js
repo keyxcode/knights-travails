5ea592d1e4efe29b67c77a3f401e8fa010b898bb
keyxcode <keyxcode@gmail.com>
2023-03-25 13:44:10 +0700

    Add test cases to driver

--- driver.js
+++ driver.js
@@ -7,4 +7,10 @@ const knightMoves = knightsModule.knightMoves;
 // console.log(possibleNextLocations([2, 1]));
 
 // Test knight moves
-console.log(knightMoves([2, 1], [7, 7]));
+console.log(knightMoves([2, 1], [7, 7])); // expect 5 moves
+console.log(knightMoves([2, 5], [5, 0])); // expect 4 moves
+console.log(knightMoves([3, 4], [5, 6])); // expect 4 moves
+console.log(knightMoves([5, 2], [0, 4])); // expect 3 moves
+console.log(knightMoves([7, 5], [1, 4])); // expect 3 moves
+console.log(knightMoves([0, 6], [0, 6])); // expect 0 moves
+console.log(knightMoves([1, 3], [2, 1])); // expect 1 moves
